e549e66a17102c60adb0f2bdc439cf81910214ff
Ryan <rlorenz@uco.edu>
2020-09-07 21:47:33 -0500

    initial model setup

---
+++ test/BaseballGameTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import model.BaseballGame;
+
+public class BaseballGameTest {
+
+	public static void main(String[] args) {
+
+		final int MAX = 5000;
+
+		for (int i = 0; i < MAX; i++) {
+			BaseballGame b = new BaseballGame();
+			assert checkKey(b.getKey());
+		}
+	}
+
+	private static boolean checkKey(int[] key) {
+		if (key[0] == key[1] || key[0] == 2 || key[1] == key[2]) {
+			return false;
+		}
+		return true;
+	}
+
+}
